f00600850a9c28b3ab1cd9038a2b84b0221fd3d7
孙继峰 <sun.jifeng@outlook.com>
2020-10-25 15:18:37 +0800

    Observer Pattern register and notify

--- src/main/java/weathermachine/WeatherData.java
+++ src/main/java/weathermachine/WeatherData.java
@@ -1,9 +1,18 @@
 package weathermachine;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class WeatherData {
 
-    public void measurementsChanged(int temp, int humidity, int windPower) {
+    private Set<MeasurementListener> listeners = new HashSet<>();
 
+    public void measurementsChanged(int temp, int humidity, int windPower) {
+        listeners.forEach(listener -> listener.onChange(temp, humidity, windPower));
+    }
+
+    public void register(MeasurementListener listener) {
+        this.listeners.add(listener);
     }
 }
 
